acdb3e482796b5d91978a4760523f1a5e6f654f9
thomas Loubiou <xabufr@gmail.com>
2012-03-31 18:37:50 +0200

    correction bug audio

--- src/engines/sound/soundengine.cpp
+++ src/engines/sound/soundengine.cpp
@@ -15,11 +15,12 @@ unsigned int SoundEngine::PlaySound(const std::string& chemin)
 {
     m_sounds[m_last] = SoundParams();
     sf::Sound *s = new sf::Sound;
-    m_sounds[m_last].sound = new sf::Sound;
+    m_sounds[m_last].sound = s;
     s->SetRelativeToListener(true);
     s->SetPosition(0,0,0);
     s->SetBuffer(*SoundBufferManager::GetInstance()->Get(chemin));
     s->Play();
+
     return m_last++;
 }
 unsigned int SoundEngine::PlayLocalizedSound(const std::string& chemin)
